5a457bddffcf3264260e99bb33cd0b613df51155
Sicheng Chen <csc0330@hotmail.com>
2025-08-04 23:51:55 +0800

    feat: Add tools for university directory, events, food trucks, library, merchants, recreation sports, and student organizations

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,20 @@ import {
   ListToolsRequestSchema,
 } from "@modelcontextprotocol/sdk/types.js";
 
+import { athleticsTools, handleAthleticsTool } from "./athletics.js";
 import { busTools, handleBusTool } from "./bus.js";
+import { buildingTools, handleBuildingTool } from "./buildings.js";
+import { calendarTools, handleCalendarTool } from "./calendar.js";
 import { classTools, handleClassTool } from "./classes.js";
 import { diningTools, handleDiningTool } from "./dining.js";
+import { directoryTools, handleDirectoryTool } from "./directory.js";
+import { eventsTools, handleEventsTool } from "./events.js";
+import { foodtruckTools, handleFoodtruckTool } from "./foodtrucks.js";
+import { libraryTools, handleLibraryTool } from "./library.js";
+import { merchantTools, handleMerchantTool } from "./merchants.js";
 import { parkingTools, handleParkingTool } from "./parking.js";
+import { recsportsTools, handleRecsportsTool } from "./recsports.js";
+import { studentorgTools, handleStudentorgTool } from "./studentorgs.js";
 
 const server = new Server(
   {
@@ -25,10 +35,20 @@ const server = new Server(
 );
 
 const allTools = [
+  ...athleticsTools,
   ...busTools,
+  ...buildingTools,
+  ...calendarTools,
   ...classTools, 
   ...diningTools,
-  ...parkingTools
+  ...directoryTools,
+  ...eventsTools,
+  ...foodtruckTools,
+  ...libraryTools,
+  ...merchantTools,
+  ...parkingTools,
+  ...recsportsTools,
+  ...studentorgTools
 ];
 
 server.setRequestHandler(ListToolsRequestSchema, async () => {
@@ -41,8 +61,20 @@ server.setRequestHandler(CallToolRequestSchema, async (request) => {
   const { name, arguments: args } = request.params;
 
   try {
+    if (athleticsTools.some(tool => tool.name === name)) {
+      return await handleAthleticsTool(name, args || {});
+    }
+    
     if (busTools.some(tool => tool.name === name)) {
       return await handleBusTool(name, args || {});
+    }
+    
+    if (buildingTools.some(tool => tool.name === name)) {
+      return await handleBuildingTool(name, args || {});
+    }
+    
+    if (calendarTools.some(tool => tool.name === name)) {
+      return await handleCalendarTool(name, args || {});
     }
     
     if (classTools.some(tool => tool.name === name)) {
@@ -53,8 +85,36 @@ server.setRequestHandler(CallToolRequestSchema, async (request) => {
       return await handleDiningTool(name, args || {});
     }
     
+    if (directoryTools.some(tool => tool.name === name)) {
+      return await handleDirectoryTool(name, args || {});
+    }
+    
+    if (eventsTools.some(tool => tool.name === name)) {
+      return await handleEventsTool(name, args || {});
+    }
+    
+    if (foodtruckTools.some(tool => tool.name === name)) {
+      return await handleFoodtruckTool(name, args || {});
+    }
+    
+    if (libraryTools.some(tool => tool.name === name)) {
+      return await handleLibraryTool(name, args || {});
+    }
+    
+    if (merchantTools.some(tool => tool.name === name)) {
+      return await handleMerchantTool(name, args || {});
+    }
+    
     if (parkingTools.some(tool => tool.name === name)) {
       return await handleParkingTool(name, args || {});
+    }
+    
+    if (recsportsTools.some(tool => tool.name === name)) {
+      return await handleRecsportsTool(name, args || {});
+    }
+    
+    if (studentorgTools.some(tool => tool.name === name)) {
+      return await handleStudentorgTool(name, args || {});
     }
 
     throw new Error(`Unknown tool: ${name}`);
